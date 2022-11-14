51371b20d2135c10b5d0e3b3fb4700bd73eaca5f
Olga Maciaszek-Sharma <olga.maciaszek@gmail.com>
2022-11-14 13:20:51 +0100

    Init.

---
+++ README.md
@@ -0,0 +1,5 @@
+# spring-one-tour-tel-aviv-interface-clients
+
+Interact with the demo:
+
+`http POST :9081/register < person.json`
---
+++ customer-service-spring-framework-6/src/test/java/com/example/spring/one/telaviv/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.spring.one.telaviv.customerservice;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
